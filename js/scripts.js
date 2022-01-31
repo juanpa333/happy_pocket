11558e9fdfe7e14eab7b258ca5e492a7dd3662f2
juan <gomezjpgmail.com>
2022-01-31 15:37:09 -0500

    cambios

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,9 @@
 
 
 // Hacer aparecer la barra de navegración, cuando el link esté en la parte superior de la pagina
-$(document).ready(function(){
-  $("#barranav1").hide();
-  });
 
 
  
-  $(document).scroll(function(){
-     $("#barranav1").slideUp();
-  });
+  
 
   
-$(document).ready(function(){
-  $("#colppal").mouseenter(function(){
-  	     $("#barranav1").slideDown();
-  });
-  });
