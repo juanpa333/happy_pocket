36e8e5adb7dbc375dade8592d35e95881c2e62c8
juan <gomezjpgmail.com>
2022-02-27 11:30:16 -0500

    botones y blog

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,64 @@
 
 
 
-// Hacer aparecer la barra de navegración, cuando el link esté en la parte superior de la pagina
+// cambiar de color cuando  el mouse entre al area de boton rounded pill del blog
 
+$(document).ready(function(){
+    $("#pildora1").mouseenter(function(){
+      $(this).css({"background-color": "darkblue", "color": "white"});
+    });
+  });
 
+  $(document).ready(function(){
+    $("#pildora1").mouseleave(function(){
+      $(this).css({"background-color": "white", "color": "darkblue"});
+    });
+  });
 
- 
- 
+  $(document).ready(function(){
+    $("#pildora2").mouseenter(function(){
+      $(this).css({"background-color": "darkblue", "color": "white"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora2").mouseleave(function(){
+      $(this).css({"background-color": "white", "color": "darkblue"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora3").mouseenter(function(){
+      $(this).css({"background-color": "darkblue", "color": "white"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora3").mouseleave(function(){
+      $(this).css({"background-color": "white", "color": "darkblue"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora4").mouseenter(function(){
+      $(this).css({"background-color": "darkblue", "color": "white"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora4").mouseleave(function(){
+      $(this).css({"background-color": "white", "color": "darkblue"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora5").mouseenter(function(){
+      $(this).css({"background-color": "darkblue", "color": "white"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pildora5").mouseleave(function(){
+      $(this).css({"background-color": "white", "color": "darkblue"});
+    });
+  });
