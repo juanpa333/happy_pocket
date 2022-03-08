6563e3c31c932460898ab80579809876d33bd255
juan <gomezjpgmail.com>
2022-03-08 16:51:10 -0500

    correo

--- php/prueba_mail.php
+++ php/prueba_mail.php
@@ -1,31 +1,5 @@
 <?php
-// Start with PHPMailer class
-use PHPMailer\PHPMailer\PHPMailer;
-require_once 'vendor/autoload.php';
-// create a new object
-$mail = new PHPMailer();
-// configure an SMTP
-$mail->isSMTP();
-$mail->Host = 'smtp.mailtrap.io';
-$mail->SMTPAuth = true;
-$mail->Username = '96c4b694dd0f22';
-$mail->Password = '0546543dcdab86';
-$mail->SMTPSecure = 'tls';
-$mail->Port = 2525;
-$mail->setFrom('eb27c9c1-a148-4fa4-bcae-2a8d4993d166@heroku.com', 'Your Hotel');
-$mail->addAddress('gomezjp@gmail.com', 'Me');
-$mail->Subject = 'Thanks for choosing Our Hotel!';
-// Set HTML
-$mail->isHTML(TRUE);
-$mail->Body = '<html>Hi there, we are happy to <br>confirm your booking.</br> Please check the document in the attachment.</html>';
-$mail->AltBody = 'Hi there, we are happy to confirm your booking. Please check the document in the attachment.';
-// add attachment
-$mail->addAttachment('//confirmations/yourbooking.pdf', 'yourbooking.pdf');
-// send the message
-if(!$mail->send()){
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
+
     echo 'Message has been sent';
 }
 ?>
