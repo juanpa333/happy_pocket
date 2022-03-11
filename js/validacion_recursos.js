be5980adf95384a83e36b790a744409eac82c8e1
juan <gomezjpgmail.com>
2022-03-10 22:10:30 -0500

    modal recursos

--- js/validacion_recursos.js
+++ js/validacion_recursos.js
@@ -1,21 +1,53 @@
 
+    $(document).ready(function(){
+       $("#link_recurso1_modal").hide();
+       $("#validador_nombre1").hide();
+       $("#validador_correo1").hide();
+      
+   $("#boton_envio_datos").click(function(){
+    $("#validador_correo1").hide();
+    $("#validador_nombre1").hide();
+    let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let nombre1 = $("#entrada_formulario_nombre1").val();
+    let correo1 = $("#entrada_formulario_correo1").val();
+     
+    if(!(correo1.match(mailformat)))
+{
+  $("#validador_correo1").show();
+}
+      
+  if(nombre1 == null || nombre1 == "") {
+
+    $("#validador_nombre1").show();
+  }
+
+  if(!(correo1.match(mailformat)) || (nombre1 == null || nombre1 == "") )
+
+  {}
+  else{
+  $("#link_recurso1_modal").fadeIn(1000); }
+    //$("#link_recurso1_modal").removeClass("d-none");
+   
+  
+  
+     });
+   });
+    
+    
     // Validar que se ingrese nombre sin caracteres especiales
 
     // Validar que se ingrese  @ en el campo del email
 
     // validar que si este chequeada caasilla de terminos y condiciones
 
+    // Recoger los datos en un json
+
+    // Enviar JSON a la base de datos
+
+    // Habilitar link de descarga
+
 
     
-    $(document).ready(function(){
-  $("#boton_formulario").click(function(){
-    $(this).css({"background-color": "darkblue", "color": "white"});
-   
-  $("#parrafo_prueba").text($("#validationServer01").val());
-  $("#validationServer03").removeClass("is-invalid").addClass("is-valid");
- //  $("#validationServer03").addClass("is-valid");
-  $("#validationServer03Feedback").removeClass("invalid-feedback").addClass("valid-feedback");
-  // $("#validationServer03Feedback").addClass("valid-feedback");
+    
 
-  });
-});
+  
