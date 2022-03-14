3d0fb2c0af6b4faffa131f41c02267c68c3081aa
juan <gomezjpgmail.com>
2022-03-13 22:25:52 -0500

    cambios archivos descargas

--- js/validacion_recursos.js
+++ js/validacion_recursos.js
@@ -3,10 +3,18 @@
        $("#link_recurso1_modal").hide();
        $("#validador_nombre1").hide();
        $("#validador_correo1").hide();
+       $("#link_recurso2_modal").hide();
+       $("#validador_nombre2").hide();
+       $("#validador_correo2").hide();
+       $("#link_recurso3_modal").hide();
+       $("#validador_nombre3").hide();
+       $("#validador_correo3").hide();
       
    $("#boton_envio_datos").click(function(){
     $("#validador_correo1").hide();
     $("#validador_nombre1").hide();
+
+  
     let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     let nombre1 = $("#entrada_formulario_nombre1").val();
     let correo1 = $("#entrada_formulario_correo1").val();
@@ -31,6 +39,73 @@
   
   
      });
+
+
+     $("#boton_envio_datos2").click(function(){
+      $("#validador_correo2").hide();
+      $("#validador_nombre2").hide();
+  
+    
+      let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      let nombre1 = $("#entrada_formulario_nombre2").val();
+      let correo1 = $("#entrada_formulario_correo2").val();
+       
+      if(!(correo1.match(mailformat)))
+  {
+    $("#validador_correo2").show();
+  }
+        
+    if(nombre1 == null || nombre1 == "") {
+  
+      $("#validador_nombre2").show();
+    }
+  
+    if(!(correo1.match(mailformat)) || (nombre1 == null || nombre1 == "") )
+  
+    {}
+    else{
+    $("#link_recurso2_modal").fadeIn(1000); }
+      //$("#link_recurso1_modal").removeClass("d-none");
+     
+    
+    
+       });
+
+
+
+
+       $("#boton_envio_datos3").click(function(){
+        $("#validador_correo3").hide();
+        $("#validador_nombre3").hide();
+    
+      
+        let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        let nombre1 = $("#entrada_formulario_nombre3").val();
+        let correo1 = $("#entrada_formulario_correo3").val();
+         
+        if(!(correo1.match(mailformat)))
+    {
+      $("#validador_correo2").show();
+    }
+          
+      if(nombre1 == null || nombre1 == "") {
+    
+        $("#validador_nombre3").show();
+      }
+    
+      if(!(correo1.match(mailformat)) || (nombre1 == null || nombre1 == "") )
+    
+      {}
+      else{
+      $("#link_recurso3_modal").fadeIn(1000); }
+        //$("#link_recurso1_modal").removeClass("d-none");
+       
+      
+      
+         });
+  
+
+
    });
     
     
