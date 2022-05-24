3e631e21a345427a1c1977360a76732bbca77ec3
juan <gomezjpgmail.com>
2022-05-23 19:02:20 -0500

    varios cambios en blog dinamico

---
+++ blog_dinamico.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/style_hp.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <title>Blog -  Happy Pocket</title>
+    <link rel="icon" type="image/png" href="recursos/icono.png">
+    <link href="css/carousel.css" rel="stylesheet">
+    <script src="js/carga_blog.js"></script>
+    
+    
+    <!-- Custom styles for this template -->
+   
+  </head>
+  <body style="margin-bottom: 0cm;padding-bottom: 0cm;" >
+
+
+   <?php
+   require "barra_sup_moviles.php";
+   ?>
+ 
+ <?php
+    require "logo_navbar_sup.php";
+    ?>
+
+<div class="row bg-light" style="height: 120px">
+  
+</div>
+
+<?php
+require "pildoras.php";
+?>
+
+<!--Empiezan Artículos del blog-->
+
+
+<div class="container mb-5">
+  <div id="cuerpo_articulos" class="row justify-content-center text-align">
+
+   
+
+    
+      
+
+    
+    
+
+  
+
+  </div>
+</div>
+
+
+
+   
+  <?php
+  require "footer.php";
+  ?>
+    
+    
+
+    </script>
+  </body>
+</html>
---
+++ js/carga_blog.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+$.ajax({
+    // la URL para la petición
+    url : 'php/carga_blog.php',
+ 
+    // la información a enviar
+    // (también es posible utilizar una cadena de datos)
+    // El valor de la derecha, es el valor de jscript (jquery)
+    // El valor de la izquierda es el nombre de la variable POST
+    data : { 
+            col1 : 'id' ,
+            col2 :  'entrada'
+           
+            },
+ 
+    // especifica si será una petición POST o GET
+    type : 'POST',
+ 
+    // el tipo de información que se espera de respuesta
+    dataType : 'json',
+ 
+    // código a ejecutar si la petición es satisfactoria;
+    // la respuesta es pasada como argumento a la función
+    success : function(json) {
+                
+        $("#cuerpo_articulos").append(json.articulos);
+       
+    },
+ 
+    // código a ejecutar si la petición falla;
+    // son pasados como argumentos a la función
+    // el objeto jqXHR (extensión de XMLHttpRequest), un texto con el estatus
+    // de la petición y un texto con la descripción del error que haya dado el servidor
+    error : function(jqXHR, status, error) {
+     alert(error);
+    },
+ 
+  //  código a ejecutar sin importar si la petición falló o no
+    complete : function(jqXHR, status) {
+       // alert('Petición realizada');
+       // alert(json.status);
+    }
+})
+
+}) //End document ready function
